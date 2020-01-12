df109009d88ee16a4d8b79a525029600710742a0
Carlos Alberto Zuñiga Nava <czuniga@kwantec.com>
2020-01-12 09:31:24 -0600

    linked login logic to ui template

--- src/DemoPages/UserPages/Login/index.js
+++ src/DemoPages/UserPages/Login/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment, Component} from "react";
+import React, { Fragment, Component } from "react";
 
 import Slider from "react-slick";
 
@@ -6,9 +6,68 @@ import bg1 from '../../../assets/utils/images/originals/city.jpg';
 import bg2 from '../../../assets/utils/images/originals/citydark.jpg';
 import bg3 from '../../../assets/utils/images/originals/citynights.jpg';
 
-import {Col, Row, Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { required, length, email } from '../../../util/validators';
 
 export default class Login extends Component {
+
+    state = {
+        loginForm: {
+            email: {
+                value: '',
+                valid: false,
+                touched: false,
+                validators: [required, email]
+            },
+            password: {
+                value: '',
+                valid: false,
+                touched: false,
+                validators: [required, length({ min: 5 })]
+            },
+            formIsValid: false
+        }
+    };
+
+    inputChangeHandler = ({ target }) => {
+        this.setState(prevState => {
+            let isValid = true;
+            for (const validator of prevState.loginForm[target.name].validators) {
+                isValid = isValid && validator(target.value);
+            }
+            const updatedForm = {
+                ...prevState.loginForm,
+                [target.name]: {
+                    ...prevState.loginForm[target.name],
+                    valid: isValid,
+                    value: target.value
+                }
+            };
+            let formIsValid = true;
+            for (const inputName in updatedForm) {
+                formIsValid = formIsValid && updatedForm[inputName].valid;
+            }
+            return {
+                loginForm: updatedForm,
+                formIsValid: formIsValid
+            };
+        });
+    };
+
+    inputBlurHandler = input => {
+        this.setState(prevState => {
+            return {
+                loginForm: {
+                    ...prevState.loginForm,
+                    [input]: {
+                        ...prevState.loginForm[input],
+                        touched: true
+                    }
+                }
+            };
+        });
+    };
+
     render() {
         let settings = {
             dots: true,
@@ -25,7 +84,7 @@ export default class Login extends Component {
         };
         return (
 
-            <Fragment>
+            <>
                 <div className="h-100">
                     <Row className="h-100 no-gutters">
                         <Col lg="4" className="d-none d-lg-block">
@@ -34,9 +93,9 @@ export default class Login extends Component {
                                     <div
                                         className="h-100 d-flex justify-content-center align-items-center bg-plum-plate">
                                         <div className="slide-img-bg"
-                                             style={{
-                                                 backgroundImage: 'url(' + bg1 + ')'
-                                             }}/>
+                                            style={{
+                                                backgroundImage: 'url(' + bg1 + ')'
+                                            }} />
                                         <div className="slider-content">
                                             <h3>Perfect Balance</h3>
                                             <p>
@@ -47,9 +106,9 @@ export default class Login extends Component {
                                     <div
                                         className="h-100 d-flex justify-content-center align-items-center bg-premium-dark">
                                         <div className="slide-img-bg"
-                                             style={{
-                                                 backgroundImage: 'url(' + bg3 + ')'
-                                             }}/>
+                                            style={{
+                                                backgroundImage: 'url(' + bg3 + ')'
+                                            }} />
                                         <div className="slider-content">
                                             <h3>Scalable, Modular, Consistent</h3>
                                             <p>
@@ -61,9 +120,9 @@ export default class Login extends Component {
                                     <div
                                         className="h-100 d-flex justify-content-center align-items-center bg-sunny-morning">
                                         <div className="slide-img-bg opacity-6"
-                                             style={{
-                                                 backgroundImage: 'url(' + bg2 + ')'
-                                             }}/>
+                                            style={{
+                                                backgroundImage: 'url(' + bg2 + ')'
+                                            }} />
                                         <div className="slider-content">
                                             <h3>Complex, but lightweight</h3>
                                             <p>
@@ -77,7 +136,7 @@ export default class Login extends Component {
                         </Col>
                         <Col lg="8" md="12" className="h-100 d-flex bg-white justify-content-center align-items-center">
                             <Col lg="9" md="10" sm="12" className="mx-auto app-login-box">
-                                <div className="app-logo"/>
+                                <div className="app-logo" />
                                 <h4 className="mb-0">
                                     <div>Welcome back,</div>
                                     <span>Please sign in to your account.</span>
@@ -86,30 +145,46 @@ export default class Login extends Component {
                                     No account?{' '}
                                     <a href="javascript:void(0);" className="text-primary">Sign up now</a>
                                 </h6>
-                                <Row className="divider"/>
+                                <Row className="divider" />
                                 <div>
-                                    <Form>
+                                    <Form
+                                        onSubmit={e =>
+                                            this.props.onLogin(e, {
+                                                email: this.state.loginForm.email.value,
+                                                password: this.state.loginForm.password.value
+                                            })
+                                        }
+                                    >
                                         <Row form>
                                             <Col md={6}>
                                                 <FormGroup>
-                                                    <Label for="exampleEmail">Email</Label>
-                                                    <Input type="email" name="email" id="exampleEmail"
-                                                           placeholder="Email here..."/>
+                                                    <Label for="email">Email</Label>
+                                                    <Input
+                                                        name='email'
+                                                        id="email"
+                                                        type="email"
+                                                        control="input"
+                                                        onChange={this.inputChangeHandler}
+                                                        onBlur={this.inputBlurHandler.bind(this, 'email')}
+                                                        value={this.state.loginForm['email'].value}
+                                                        valid={this.state.loginForm['email'].valid}
+                                                        touched={this.state.loginForm['email'].touched}
+                                                        placeholder="Email here..." />
                                                 </FormGroup>
                                             </Col>
                                             <Col md={6}>
                                                 <FormGroup>
                                                     <Label for="examplePassword">Password</Label>
                                                     <Input type="password" name="password" id="examplePassword"
-                                                           placeholder="Password here..."/>
+                                                        placeholder="Password here..." />
                                                 </FormGroup>
                                             </Col>
                                         </Row>
                                         <FormGroup check>
-                                            <Input type="checkbox" name="check" id="exampleCheck"/>
+                                            <Input type="checkbox" name="check" id="exampleCheck" />
                                             <Label for="exampleCheck" check>Keep me logged in</Label>
                                         </FormGroup>
-                                        <Row className="divider"/>
+                                        <Row className="divider" />
                                         <div className="d-flex align-items-center">
                                             <div className="ml-auto">
                                                 <a href="javascript:void(0);" className="btn-lg btn btn-link">Recover
@@ -123,7 +198,7 @@ export default class Login extends Component {
                         </Col>
                     </Row>
                 </div>
-            </Fragment>
+            </>
         );
     }
 }
